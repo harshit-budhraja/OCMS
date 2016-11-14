70991223d8efaeb1bf59ef7bd2ed7218b9c0f50c
harshitbudhraja <harshitbudhraja1301@gmail.com>
2016-11-14 23:31:22 +0530

    Upload the actual CRIMER app

---
+++ CRIMER/app/src/main/java/com/arachnisapps/crimer/MyFirebaseInstanceIDService.java
@@ -0,0 +1,31 @@
+package com.arachnisapps.crimer;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by arachnis on 11/11/16.
+ */
+//Class extending FirebaseInstanceIdService
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
